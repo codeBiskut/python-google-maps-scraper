803e10b21afef15fe6db08b4efdc8aa2c0286d2c
Joshua Rinehart <josh.rinehart.2k@gmail.com>
2023-09-14 09:46:12 -0400

    added soup and requests to interact with google maps search results

--- scraper.py
+++ scraper.py
@@ -3,7 +3,32 @@ from bs4 import BeautifulSoup
 
 def google_search(query):
     base_url = 'https://www.google.com/maps/search/'
-    
+    params = {
+        'q': query,
+        'hl': 'en' # set the language to return
+    }
+
+    headers = {
+        'User-Agent': 'MoziMozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+
+    response = requests.get(base_url, params=params,headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        results = []
+
+        for item in soup.select('.tF2Cxc'):
+            title = item.h3.get_text()
+            link = item.a['href']
+            snippet = item.select_one('.aCOpRe').get_text()
+            results.append({'title': title, 'link': link, 'snippet': snippet})
+
+        return results
+
+    else:
+        print('Error:', response.status_code)
+        return None
 
 def main():
     search_query = 'kayaks near me'
