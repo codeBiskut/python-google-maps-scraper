29077730ac63311b02dee0d09ea60b9d5714eaa3
Joshua Rinehart <josh.rinehart.2k@gmail.com>
2023-09-17 19:20:39 -0400

    json response is now translated into place objects to be read or exported

--- scraper.py
+++ scraper.py
@@ -2,9 +2,9 @@ import requests
 import json
 import csv
 
-# $ python -m venv project_venv     --creates virtual environment
-# $ source project_venv/bin/activate    --activates virtual environment
-# (project_venv) $ deactivate       --deactivates virtual environment
+# python -m venv project_venv         --creates virtual environment
+# source project_venv/bin/activate    --activates virtual environment
+# (project_venv) $deactivate          --deactivates virtual environment
 
 
 def google_search(query):
@@ -20,9 +20,32 @@ def google_search(query):
     response = requests.request("POST", url, json=payload, headers=headers)
 
     if response.status_code == 200:
-        
+        #parse json response into list of dictionaries
+        place_data = response.json()
 
-        return results
+        #class for objects
+        class Place:
+            def __init__(self, name, address, number, rating, website):
+                self.name = name
+                self.address = address
+                self.number = number
+                self.rating = rating
+                self.website = website
+
+        #list of places
+        place_objects = []
+
+        #iterate through dictionary lists and create objects
+        for place in place_data.get("places", []):
+            name = place.get('displayName.text')
+            address = place.get('formattedAddress')
+            number = place.get('nationalPhoneNumber')
+            rating = place.get('rating')
+            website = place.get('websiteUri')
+            place = Place(name, address, number, rating, website)
+            place_objects.append(place)
+
+        return place_objects
 
     else:
         print('Error:', response.status_code)
@@ -33,15 +56,16 @@ def main():
     search_results = google_search(search_query)
 
     if search_results:
-        for i, result in enumerate(search_results, start=1):
-            print(f"Result #{i}:")
-            print(f"Title: {result['title']}")
-            print(f"Link: {result['link']}")
-            print(f"Snippet: {result['snippet']}")
+        for result in search_results:
+            print(f"Name: {result.name}")
+            print(f"Address: {result.address}")
+            print(f"Number: {result.number}")
+            print(f"Rating: {result.rating}")
+            print(f"Website: {result.website}")
             print('\n')
 
-    # else:
-    #     print('no results')
+    else:
+        print('no results')
 
 if __name__ == '__main__':
     main()
