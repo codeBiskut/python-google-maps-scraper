493af5fa4ce71bbc8044d644643119d2e0379f30
Joshua Rinehart <josh.rinehart.2k@gmail.com>
2023-09-17 20:27:06 -0400

    added dotenv file for api key security

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
-import json
 import csv
+import os
+from dotenv import load_dotenv
 
 # python -m venv project_venv         --creates virtual environment
 # source project_venv/bin/activate    --activates virtual environment
@@ -8,13 +9,16 @@ import csv
 
 
 def google_search(query):
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+
     url = "https://places.googleapis.com/v1/places:searchText"
 
-    payload = {"textQuery": "kayaks near me"}
+    payload = {"textQuery": "breweries near me"}
     headers = {
         "Content-Type": "application/json",
         "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.websiteUri,places.nationalPhoneNumber,places.rating",
-        "X-Goog-Api-Key": "AIzaSyD2m5kAcnJsPseSkz2CKlZcOF5ijIhQSIM"
+        "X-Goog-Api-Key": api_key
     }
 
     response = requests.request("POST", url, json=payload, headers=headers)
@@ -37,7 +41,7 @@ def google_search(query):
 
         #iterate through dictionary lists and create objects
         for place in place_data.get("places", []):
-            name = place.get('displayName.text')
+            name = place.get('displayName', {}).get('text')
             address = place.get('formattedAddress')
             number = place.get('nationalPhoneNumber')
             rating = place.get('rating')
@@ -52,7 +56,7 @@ def google_search(query):
         return None
 
 def main():
-    search_query = 'kayaks near me'
+    search_query = 'breweries'
     search_results = google_search(search_query)
 
     if search_results:
